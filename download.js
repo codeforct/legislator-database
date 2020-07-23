5ff7f069ef6c2f491fb3a7697b54ec06646cd241
José Padilla <jpadilla@webapplicate.com>
2020-07-23 08:41:32 -0400

    Download data file from legislator-database-api.glitch.me

---
+++ download.js
@@ -0,0 +1,14 @@
+const fsp = require('fs').promises;
+const axios
+
+axios.get('https://legislator-database-api.glitch.me')
+  .then((data) => {
+    return fsp.writeFile('data/latest.json', JSON.stringify(data));
+  })
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
